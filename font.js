91cd20eb4f5909cb818683e3633ef346ae3095cf
kahlidsattar001-afk <kahlidsattar001@gmail.com>
2025-09-20 14:47:38 +0500

    Update font.js

--- font.js
+++ font.js
@@ -3,4 +3,7 @@
 // Array.from(sec).forEach(element => {
 //     element.style.display = "none";
 // });
+
+document.querySelectorAll("#MainContent").style.display = "none";
+
 console.log("tary to l lag gay")
