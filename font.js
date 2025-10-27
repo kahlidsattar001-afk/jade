361bef64e60dde0bd5a11c441624a19dceedcef2
kahlidsattar001-afk <kahlidsattar001@gmail.com>
2025-10-27 23:01:24 +0500

    Update font.js

--- font.js
+++ font.js
@@ -1,12 +1,1 @@
-// Hide all <section> elements
-document.querySelectorAll("section").forEach(element => {
-    element.style.display = "none";
-});
 
-// Hide element with ID #MainContent
-const mainContent = document.querySelector("#MainContent");
-if (mainContent) {
-    mainContent.style.display = "none";
-}
-
-console.log("✅Lory lag gay successfully.");
